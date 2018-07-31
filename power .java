33a4a1a2a6e1fff296ff7556b5ef267895d23624
vijayalakshmi121997 <41734864+vijayalakshmi121997@users.noreply.github.com>
2018-07-31 10:40:55 +0530

    Create power .java

---
+++ power .java
@@ -0,0 +1,10 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int base = 3, exponent = -4;
+        double result = Math.pow(base, exponent);
+ 
+        System.out.println("Answer = " + result);
+	}
+}
