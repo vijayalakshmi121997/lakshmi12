6b8f6f8743102b2acb563a15795988e39866de26
vijayalakshmi121997 <41734864+vijayalakshmi121997@users.noreply.github.com>
2018-07-31 10:08:35 +0530

    Create naturalnumbers.java

---
+++ naturalnumbers.java
@@ -0,0 +1,15 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		{
+		int sum = 0;
+		int n=10;
+		for(int i = 1;i <= n;i++)
+		{
+			sum = sum + i;
+		}
+		System.out.println("The Sum Of "+n+" Numbers are:" + sum);
+		}
+	}
+}
